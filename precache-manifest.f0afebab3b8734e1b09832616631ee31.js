2628f43aa6d7108b082f7c2e51f0da063bfe5b66
kim00425 <kim00425@gmail.com>
2019-01-24 00:16:06 +0900

    Updates

---
+++ precache-manifest.f0afebab3b8734e1b09832616631ee31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "70a061c28de77fe63354",
+    "url": "/movie_app/static/js/main.70a061c2.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/movie_app/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "70a061c28de77fe63354",
+    "url": "/movie_app/static/css/main.51cf1f00.chunk.css"
+  },
+  {
+    "revision": "ab4577500768dd834364d9a370e267a8",
+    "url": "/movie_app/index.html"
+  }
+];
